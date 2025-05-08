a3d6f4853d46f7f0c7f5bf05c1dcfa10d795367b
ismailaydemir <67390681+ismailaydemirx@users.noreply.github.com>
2025-05-08 19:56:02 +0300

    Add project files.

---
+++ QAWebAutomationTests/Pages/LoginPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace QAWebAutomationTests.Pages
+{
+
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver webDriver)
+        {
+            this.driver = webDriver;
+        }
+
+        private IWebElement UsernameField => driver.FindElement(By.Id("user-name"));
+        private IWebElement PasswordField => driver.FindElement(By.Id("password"));
+        private IWebElement LoginButton => driver.FindElement(By.Id("login-button"));
+
+        public void Login(string username, string password)
+        {
+            UsernameField.SendKeys(username);
+            PasswordField.SendKeys(password);
+            LoginButton.Click();
+        }
+    }
+
+}
---
+++ QAWebAutomationTests/Utils/Driver.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace QAWebAutomationTests.Utils
+{
+    public static class Driver
+    {
+        public static IWebDriver Instance { get; private set; }
+
+        public static void Initialize()
+        {
+            Instance = new ChromeDriver();
+            Instance.Manage().Window.Maximize();
+        }
+
+        public static void Quit()
+        {
+            Instance.Quit();
+        }
+    }
+
+}
